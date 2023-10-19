91007f72ff947abea83371c17543df00632c19c3
valentindush <codesvalentin@gmail.com>
2023-10-19 08:51:37 +0200

    fix: port and start command for deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api-docs', app, document)
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
